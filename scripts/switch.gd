32b1d50f6a7106592556e7f86520d7e0ab827efa
Patrick <patrick56hughes@gmail.com>
2025-04-20 13:11:59 -0400

    Switch script now reads the interatcion range from the size of its collision shape

--- scripts/switch.gd
+++ scripts/switch.gd
@@ -4,22 +4,22 @@ extends Area2D
 
 @onready var game_manager: Node2D = %Game_Manager
 @onready var face: MeshInstance2D = $Face
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 @export var state:bool = false
 
+@onready var range:int = collision_shape.shape.radius
 
 func _ready() -> void:
-# Im not sure why this doesnt work
-# Connects the Game Manager to the Switch
-	#game_manager.switch.connect(_on_switch)
-	pass
 # Setting Initial Color
 	setColor()
+	#print(collision_shape.shape.radius)
+	
 
 func _on_switch(origin:Vector2):
 	#print("Switch Signal Received")
 	#print(origin, position, abs(position.distance_to(origin)))
-	if abs(position.distance_to(origin)) <= 24:
+	if abs(position.distance_to(origin)) <= range:
 		state = !state
 		setColor()
 	#receiver.trigger(state)
