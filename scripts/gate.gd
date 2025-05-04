18163627dac03fae68dcf8e031593de23de3730d
Patrick <patrick56hughes@gmail.com>
2025-05-04 11:31:21 -0400

    You can now choose the default state of a gate

--- scripts/gate.gd
+++ scripts/gate.gd
@@ -3,17 +3,29 @@ extends Node2D
 @onready var collider: CollisionShape2D = $CollisionShape2D
 
 @export var trigger:Node
-@export var state:bool = false:
+var trigger_state:bool :
 	get():
-		return !trigger.state
+		return trigger.state
+
+@export var is_inverted:bool
+# If NOT inverted: Gate is ON when switch is OFF
+# If IS inverted:  Gate is OFF when switch is ON
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	if state:
-		visible = true
-		collider.disabled = false
+	if is_inverted:
+		if trigger_state:
+			visible = true
+			collider.disabled = false
+		else:
+			visible = false
+			collider.disabled = true
 	else:
-		visible = false
-		collider.disabled = true
+		if trigger_state:
+			visible = false
+			collider.disabled = true
+		else:
+			visible = true
+			collider.disabled = false
